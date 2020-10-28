791916d68b85a720fe715906c97a957bf9c5c1ab
Esteban Ramirez <ramirezesteban91@gmail.com>
2020-10-28 20:12:48 -0300

    second commit

--- promedio2.rb
+++ promedio2.rb
@@ -4,6 +4,6 @@ user2=[2,3,1,4]
 def compara_arrays(user1,user2)
   p1=user1.inject{|sum,e|sum+e}/user1.count.to_f
   p2=user2.inject{|sum,e|sum+e}/user2.count.to_f
-  p1 > p2 ? "user1 tiene mayor promedio #{p1}" : "user2 tiene mayor #{p2}"
+  p1 > p2 ? "user1 tiene mayor promedio #{p1}" : "user2 tiene mayor promedio #{p2}"
 end
 puts compara_arrays(user1,user2)
